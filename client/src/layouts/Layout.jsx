799936411ef345504a9f270d0ccbf3f0f7b5ebfb
laces <sneakerheadsean@gmail.com>
2021-10-06 20:15:00 -0400

    updates

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,26 +1,28 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 export default function Layout(props) {
   return (
     <>
       <header>
-        <h1>SKu</h1>
+        <Link to="/" exact>
+          <h1>SKu</h1>
+        </Link>
         {props.currentUser ? (
           <div>
-            <p>{ props.currentUser.username }</p>
+            <p>{props.currentUser.username}</p>
             <button onClick={props.handleLogout}>Logout</button>
           </div>
         ) : (
-            <Link to='/login'>Login/Register</Link>
+          <Link to="/login">Login/Register</Link>
         )}
         <hr />
         {props.currentUser && (
           <div>
-            <Link to='/sneakers'>Sneakers</Link>
+            <Link to="/sneakers">Sneakers</Link>
           </div>
         )}
       </header>
       {props.children}
-      </>
-  )
+    </>
+  );
 }
