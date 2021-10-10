d3ec7c79d713d3d3ef549d523d0368562f4bd49f
laces <sneakerheadsean@gmail.com>
2021-10-10 13:36:06 -0400

    Styling

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -9,14 +9,21 @@ const Home = () => {
     
 
         <div className="home-image">
-          <img src="https://media.gq.com/photos/60d21930430ae505071c3806/16:9/w_2560%2Cc_limit/SNEAKER_GUIDE_OPENER.jpg" alt="" id="image-home" />
+          <img src="https://cdn.sanity.io/images/c1chvb1i/production/1d98ecb3e9f1f6454adb262c2f703d34326b6fd5-1100x734.jpg?rect=0,57,1100,619&w=1100&h=619" alt="" id="image-home" />
       
       </div>
 
       <div className="home-description">
           <p id="sku-description">
-            Sku description
+            <h1>
+          What is SKu?
+            </h1>
+            <hr />
+            SKu is an inventory app for all avid sneaker collectors!
+            <hr />
+            <p>
             Invisible pockets lightweight non-crease concealed zip polyester cool hand wash crochet lace. Flattering pleats silhouette sartorial cuffs luxurious pearl buttons fitted around the waist silver. Suede fabric pointed toe stylish cut front low heel sharp on-trend toe point raised heel drop sides unique silhouette. Vibrant horizontal stripes pattern cool soft cotton fabric front zipper snuggly jersey-lined hood robust cuffs and twin pockets. Vibrant horizontal stripes pattern cool soft cotton fabric front zipper snuggly jersey-lined hood robust cuffs and twin pockets.
+            </p>
           </p>
       </div>
       </div>
